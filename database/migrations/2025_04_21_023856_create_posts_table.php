6fe670e20953b8bead903c13350223cb44599dca
paulnyabaro <paulnyabaro1@gmail.com>
2025-04-21 06:26:42 +0300

    Updated database seeder

--- database/migrations/2025_04_21_023856_create_posts_table.php
+++ database/migrations/2025_04_21_023856_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('images')->nullable();
+            $table->string('image')->nullable();
             $table->string('title');
             $table->string('slug')->unique();
             $table->longText('content');
