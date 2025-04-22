9e8d079d46407867164e62a2cd5038e411ca8e4d
paulnyabaro <paulnyabaro1@gmail.com>
2025-04-22 05:49:45 +0300

    Working with components

---
+++ resources/views/components/post-item.blade.php
@@ -0,0 +1,5 @@
+<div class="p-4 border rounded mt-2 bg-gray-50 hover:bg-white">
+    <h3><b>{{ $post->title }}</b></h3>
+    <p>{{ Str::words($post->content, 20) }}</p>
+    <a href="" class="py-2 px-3 rounded bg-blue-100 hover:bg-blue-200 text-blue-800 mt-2 inline-flex">Read More</a>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,14 +16,10 @@
                     @if($posts)
                     <h2><b>Posts</b></h2>
                     @foreach ($posts as $post)
-                        <div class="p-4 border rounded mt-2 bg-gray-50 hover:bg-white">
-                            <h3><b>{{ $post->title }}</b></h3>
-                            <p>{{ Str::words($post->content, 20) }}</p>
-                            <a href="" class="py-2 px-3 rounded bg-blue-100 hover:bg-blue-200 text-blue-800 mt-2 inline-flex">Read More</a>
-                        </div>
+                        <x-post-item :post="$post" />
                     @endforeach
 
-                    {{ $posts->links() }}
+                    {{ $posts->onEachSide(1)->links() }}
                     @endif
                 </div>
             </div>
