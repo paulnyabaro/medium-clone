2bc5f6067eaf905aa479b4431c8310e03ea52de2
paulnyabaro <paulnyabaro1@gmail.com>
2025-04-22 06:21:30 +0300

    Updated index page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     {
         
         $posts = Post::orderBy('created_at', 'DESC')->paginate(10);
-        return view("dashboard", [
+        return view("post.index", [
             "posts" => $posts,
         ]);
     }
