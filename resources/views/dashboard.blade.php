3245109b734306dc0a32a15a0b91887eaca22143
paulnyabaro <paulnyabaro1@gmail.com>
2025-04-22 05:23:46 +0300

    Working with tailwind classes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,10 +16,10 @@
                     @if($posts)
                     <h2><b>Posts</b></h2>
                     @foreach ($posts as $post)
-                        <div class="p-4 bg-white border rounded mt-2">
+                        <div class="p-4 border rounded mt-2 bg-gray-50 hover:bg-white">
                             <h3><b>{{ $post->title }}</b></h3>
                             <p>{{ Str::words($post->content, 20) }}</p>
-                            <a href="" class="p-3 rounded bg-blue-700 mt-2 inline-flex">Read More</a>
+                            <a href="" class="py-2 px-3 rounded bg-blue-100 hover:bg-blue-200 text-blue-800 mt-2 inline-flex">Read More</a>
                         </div>
                     @endforeach
                     @endif
